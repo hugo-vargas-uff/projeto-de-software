e2bf2a899c710c1b888ddab0b096bcf2cb830c03
hugo-vargas-uff <hugovargas@id.uff.br>
2026-09-16 12:28:34 -0300

    test: adiciona test_reserva

---
+++ tests/unit/domain/test_reserva.py
@@ -0,0 +1,29 @@
+from airline.domain.model import Reserva, StatusReserva
+
+def test_deve_criar_reserva_confirmada():
+    voo_id = "voo-123"
+    passageiro_id = "passageiro-123"
+
+    reserva = Reserva(
+        voo_id=voo_id,
+        passageiro_id=passageiro_id,
+    )
+
+    assert reserva.voo_id == voo_id
+    assert reserva.passageiro_id == passageiro_id
+    assert reserva.status == StatusReserva.CONFIRMADA
+
+def test_deve_cancelar_uma_reserva():
+    voo_id = "voo-123"
+    passageiro_id = "passageiro-123"
+
+    reserva = Reserva(
+        voo_id=voo_id,
+        passageiro_id=passageiro_id,
+    )
+
+    reserva.cancelar()
+
+    assert reserva.voo_id == voo_id
+    assert reserva.passageiro_id == passageiro_id
+    assert reserva.status == StatusReserva.CANCELADA
