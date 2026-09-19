20fb31b167dd59947cc81c1921ae001bea9d84e8
Matheus Verdan <verdanmatheusvv@gmail.com>
2026-09-18 23:57:19 -0300

    test: cria teste de criacao de Trecho

--- tests/unit/domain/test_voo.py
+++ tests/unit/domain/test_voo.py
@@ -1,7 +1,12 @@
-from airline.domain.model import Voo, StatusVoo
+from airline.domain.model import Voo, StatusVoo, Trecho
 
 def test_deve_criar_voo_agendado():
     voo = Voo(numero_voo="MV-2019")
 
     assert voo.numero_voo == "MV-2019"
     assert voo.status == StatusVoo.AGENDADO
+
+def test_deve_criar_trecho():
+    trecho = Trecho(origem="GRU", destino="GIG")
+    assert trecho.origem == "GRU"
+    assert trecho.destino == "GIG"
