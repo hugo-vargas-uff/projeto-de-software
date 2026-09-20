42a4d6b02ee83cf46e3b6859fb470e605eee4551
Matheus Verdan <verdanmatheusvv@gmail.com>
2026-09-20 05:43:22 -0300

    test: att voo com trecho e add teste de igualdade de rota

--- tests/unit/domain/test_voo.py
+++ tests/unit/domain/test_voo.py
@@ -1,12 +1,22 @@
 from airline.domain.model import Voo, StatusVoo, Trecho
 
 def test_deve_criar_voo_agendado():
-    voo = Voo(numero_voo="MV-2019")
+    trecho_ida = Trecho(origem="RJ", destino="SP")
+    voo = Voo(numero_voo="MV-2019", trecho=trecho_ida)
 
     assert voo.numero_voo == "MV-2019"
+    assert voo.trecho.origem =="RJ"
+    assert voo.trecho.destino =="SP"
     assert voo.status == StatusVoo.AGENDADO
 
 def test_deve_criar_trecho():
-    trecho = Trecho(origem="GRU", destino="GIG")
-    assert trecho.origem == "GRU"
-    assert trecho.destino == "GIG"
+    trecho = Trecho(origem="RJ", destino="SP")
+    assert trecho.origem == "RJ"
+    assert trecho.destino == "SP"
+
+def test_igualdade_de_trechos():
+    trecho1 = Trecho(origem="RJ", destino="SP")
+    trecho2 = Trecho(origem="RJ", destino="SP")
+    assert trecho1==trecho2 #iguais
+    trechoDif= Trecho(origem="SP", destino="SC")
+    assert trechoDif!=trecho1 #sim, sao diferentes
