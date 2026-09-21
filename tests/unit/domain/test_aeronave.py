5cfc1681c80772c300b3daf71b97bf1c24c8e568
MatheusFSD <matheusandrade.fsdesign@gmail.com>
2026-09-20 22:59:58 -0300

    test: add testes de criacao e identidade da Aeronave

---
+++ tests/unit/domain/test_aeronave.py
@@ -0,0 +1,55 @@
+from airline.domain.model import Aeronave
+
+
+def test_deve_criar_aeronave():
+    aeronave = Aeronave(
+        prefixo="PT-MVA",
+        modelo="Boeing 737",
+        capacidade=180
+    )
+
+    assert aeronave.prefixo == "PT-MVA"
+    assert aeronave.modelo == "Boeing 737"
+    assert aeronave.capacidade == 180
+
+
+def test_igualdade_de_aeronaves():
+    aeronave1 = Aeronave(
+        prefixo="PT-MVA",
+        modelo="Boeing 737",
+        capacidade=180
+    )
+
+    aeronave2 = Aeronave(
+        prefixo="PT-MVA",
+        modelo="Airbus A320",
+        capacidade=200
+    )
+
+    assert aeronave1 == aeronave2
+
+
+def test_aeronaves_diferentes():
+    aeronave1 = Aeronave(
+        prefixo="PT-MVA",
+        modelo="Boeing 737",
+        capacidade=180
+    )
+
+    aeronave2 = Aeronave(
+        prefixo="PT-MVB",
+        modelo="Boeing 737",
+        capacidade=180
+    )
+
+    assert aeronave1 != aeronave2
+
+
+def test_aeronave_nova_deve_estar_disponivel():
+    aeronave = Aeronave(
+        prefixo="PT-MVA",
+        modelo="Boeing 737",
+        capacidade=180
+    )
+
+    assert aeronave.disponivel is True
